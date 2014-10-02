cae675fee06f02861a68a8d08ec7eb958bba55f3
DanillaTech <danillatech@gmail.com>
2014-10-02 23:56:39 +0600

    Find ESI

--- popup.js
+++ popup.js
@@ -3,8 +3,8 @@ styleTemplate.innerHTML = "<div class='style-name'></div><div class='actions'><a
 
 chrome.tabs.getSelected(null, function(tab) {
 	chrome.extension.sendMessage({method: "getStyles", matchUrl: tab.url}, showStyles);
-	document.querySelector("#find-styles a").href = "http://userstyles.org/styles/browse/all/" + encodeURIComponent(tab.url);
-})+"%20esi";
+	document.querySelector("#find-styles a").href = "http://userstyles.org/styles/browse/all/" + encodeURIComponent(tab.url)+"%20esi";
+});
 
 function showStyles(styles) {
 	var installed = document.getElementById("installed");
